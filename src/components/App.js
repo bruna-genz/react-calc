95a1544d9634b27b6b9a7804a6c1b99fa7441891
Bruna <brunagenz92@gmail.com>
2020-06-25 14:59:03 -0300

    make App a functional component

--- src/components/App.js
+++ src/components/App.js
@@ -3,25 +3,14 @@ import '../assets/styles/App.css';
 import Display from './Display';
 import ButtonPannel from './ButtonPannel';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      calcResult: '0',
-    };
-  }
-
-  render() {
-    const { calcResult } = this.state;
-
-    return (
-      <div className="App">
-        <h1>Math-magicians calculator</h1>
-        <Display calcResult={calcResult} />
-        <ButtonPannel />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>Math-magicians calculator</h1>
+      <Display calcResult="0" />
+      <ButtonPannel />
+    </div>
+  );
 }
 
 export default App;

e1a2e4bdefa08f361d4bb1d0725c74c57575f92a
Bruna <brunagenz92@gmail.com>
2020-06-25 19:42:51 -0300

    create logic folder with calculate and operate files; create calculate and operate function in the respective files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import './assets/styles/scss/index.scss';
+import './styles/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>

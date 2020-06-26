2658b6edf395e53217a041e3e15b348f2af335ad
Bruna <brunagenz92@gmail.com>
2020-06-26 16:39:09 -0300

    make App a class component instead of a functional component, and add state with total, next and operation properties; add handleClick function to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div className="App">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const calcResult = calculate(this.state, buttonName);
+
+    this.setState(prevState => ({
+      total: calcResult,
+      next: calcResult,
+      operation: calcResult,
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
 }
 
 export default App;
